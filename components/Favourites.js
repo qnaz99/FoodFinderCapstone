1559a78361023dc5cfad8fe1154cf5e92d40d84a
Qareeb <qareeb.nazimudeen@ryerson.ca>
2023-02-03 23:47:12 -0500

    test commit

--- components/Favourites.js
+++ components/Favourites.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>FoodFinder</Text>
+      <Text>FoodFasdasdasdinder</Text>
       <StatusBar style="auto" />
     </View>
   );

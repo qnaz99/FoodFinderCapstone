20c629433b5be180c6e7642f9cd8cba36243d34d
Qareeb <qareeb.nazimudeen@ryerson.ca>
2023-02-17 03:10:13 -0500

    hard coded google sign in

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useFonts } from 'expo-font'
 import { TouchableOpacity } from 'react-native-web';
@@ -38,11 +38,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      {/* <Image source={Icon.logo} style={{width: 100, height: 100}} /> */}
       <ImageBackground source="https://14thlane.ro/files/pages/1/OurStory2.webp" resizeMode="cover" style={styles.image}>
         <Text style={styles.logoText}>FoodFinder</Text>
         <View style={styles.button}>
-          <TouchableOpacity  onPress={() => alert('Error connecting to Google SSO Login API')} style={{backgroundColor:'white', borderRadius: 9, height:Dimensions.get('window').height * 0.06 }}>
+          <TouchableOpacity  onPress={() => Linking.openURL('https://accounts.google.com/signin')} style={{backgroundColor:'white', borderRadius: 9, height:Dimensions.get('window').height * 0.06 }}>
               <View style={{flexDirection: 'row', paddingTop:Dimensions.get('window').height * 0.015}}>
                 <Icon name="google" color="black" size={30} style={{paddingLeft:10}}></Icon>
                 <Text style={{ paddingLeft: 15, color: 'blue'}}>

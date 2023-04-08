2ec64f104d9ac89a2c765d89e06f94dd92360767
Qareeb Nazimudeen <qareeb.nazimudeen@ryerson.ca>
2023-04-08 01:53:12 -0400

    switched from yelp to foursquare to suppport displaying menus

--- app/main.jsx
+++ app/main.jsx
@@ -9,9 +9,10 @@ import * as Location from 'expo-location';
 import axios from 'axios';
 import { Link } from "expo-router";
 import { useNavigation } from "expo-router";
+//import { Restaurant } from './restaurant.jsx';
 
 
-import { YELP_API_KEY, geolocationDbUrl } from "@env";
+import { YELP_API_KEY, FOURSQUARE_API_KEY } from "@env";
 
 const ItemSeparator = ({ title }) => (
   <View
@@ -24,99 +25,118 @@ const ItemSeparator = ({ title }) => (
 );
 
 
+function renderMenu(){
+  return Restaurant
+}
+
 
 export default function Main() {
     const navigation = useNavigation();
     const [longitude, setLongitude] = useState(null);
     const [latitude, setLatitude] = useState(null);
     const [input, setInput] = useState("");
-    const [results, setResults] = useState([])
-    const [addresses, setAddresses] = useState([])
-
-
-    // navigator.geolocation.getCurrentPosition(
-    //     (position) => {
-    //         setLong(position.coords.longitude),
-    //         setLat(position.coords.latitude)
-    //     }
-    // )
+    const [results, setResults] = useState([]);
+    const [categories, setCategories] = useState("13000");
+    const [latitudeDelta, setLatitudeDelta] = useState(0.0922);
+    const [longitudeDelta, setLongitudeDelta] = useState(0.0421);
+    const [sort, setSort] = useState("distance");
 
     useEffect(() => {(async () => {
-        // let location = await Location.getCurrentPositionAsync({});
-        // setLatitude(location.coords.latitude);
-        // setLongitude(location.coords.longitude);
-        // if (latitude && longitude) return;
+      try{
+        let location = await Location.getCurrentPositionAsync({});
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+      }
+      catch(e){
+        console.log("Unable to get location: ", e)
+      }
+        
+      if (latitude && longitude) return;
 
-
-        // const response = await axios.get("https://geolocation-db.com/json/");
-        // if (response.data && response.data.latitude && response.data.longitude) {
-        //     setLatitude(response.data.latitude);
-        //     setLongitude(response.data.longitude);  
-        // }
-        setLatitude(43.657780)
-        setLongitude(-79.380480)
-        return  
+      const response = await axios.get("https://geolocation-db.com/json/");
+      if (response.data && response.data.latitude && response.data.longitude) {
+          setLatitude(response.data.latitude);
+          setLongitude(response.data.longitude);  
+      }
+      // uncommenting these 2 lines will hardcode the location to TMU Campus
+      // setLatitude(43.657780) 
+      // setLongitude(-79.380480)
+      return  
     })();}, []);
 
     async function getResults(text){
-        const response = await axios.get("https://api.yelp.com/v3/businesses/search", {
-            headers: {
-                Authorization: `Bearer ${YELP_API_KEY}`
-            },
-            params: {
-                    term: text,
-                    latitude: latitude,
-                    longitude: longitude,
-                    categories: ['restaurants', 'bars'],
-                    radius: 5000,
-                    limit: 5
-                }
-        });
-        //console.log(response.data.businesses ? true : false)
-
-        if(response.data.businesses){
-            setResults(response.data.businesses)
-            //console.log(results[0])
-            setAddresses[response.data.businesses[0].location.address1, response.data.businesses[1].location.address1, response.data.businesses[2].location.address1, response.data.businesses[3].location.address1, response.data.businesses[4].location.address1]
+      const options = {
+        headers: {
+          accept: 'application/json',
+          Authorization: 'fsq3Fcizh8BbV7Okn7EbZKcVs+6KPtMq2lAI/vXYJf+wkHI='
+        },
+        params: {
+          query: text,
+          ll: `${latitude},${longitude}`,
+          sort,
+          limit: 5,
+          categories
         }
+      }
+      const response = await axios.get("https://api.foursquare.com/v3/places/search", options);
+      // {
+      //   headers: {
+      //       Authorization: `Bearer ${FOURSQUARE_API_KEY}`
+      //   },
+      //   params: {
+      //       term: text,
+      //       latitude: latitude,
+      //       longitude: longitude,
+      //       categories: ['restaurants', 'bars'],
+      //       radius: 5000,
+      //       limit: 5
+      //   }
+      // });
+      if(response.data.results){
+          setResults(response.data.results)
+          // console.log("results: ", results[0].coordinates)
+      }
     }
-
-    
 
   return (
     <View style={styles.container}>
         <Searchbar 
-            style={styles.searchbar} 
-            placeholder={"What are you craving?"} 
-            value={input} 
-            onChangeText={(text) => {
-                setInput(text);
-                getResults(text)
-            }}
+          style={styles.searchbar} 
+          placeholder={"What are you craving?"} 
+          value={input} 
+          onChangeText={(text) => {
+            setInput(text);
+            getResults(text)
+          }}
         />
         
       {latitude && longitude && <MapView
+        
         provider="google"
         style={{ flex: 1, width: '100%', zIndex: -1 }}
         googleMapsApiKey={"AIzaSyA3Tm7zj5CX0sEhD_wJdp6KXv2DK_LAHZc"}
         region={{latitude, longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421}}
         showsUserLocation={true}
+        rotateEnabled={false}
+        onRegionChange={(region) => {
+          setLatitudeDelta(region.latitudeDelta);
+          setLongitudeDelta(region.longitudeDelta);
+        }}
       >
-        {results[0] && <Marker title='HERE' coordinate={results[0].coordinates}></Marker>}
-        {results[0] && <Marker coordinate={results[0].coordinates}><Text>{results[0].name}</Text></Marker>}
+        {results[0] && <Marker coordinate={{latitude: results[0].geocodes.main.latitude, longitude: results[0].geocodes.main.longitude}}></Marker>}
+        {results[0] && <Marker coordinate={{latitude: results[0].geocodes.main.latitude-(latitudeDelta/35), longitude:results[0].geocodes.main.longitude}}><Text style={{fontWeight: 'bold'}}>{results[0].name}</Text></Marker>}
 
-        {results[1] && <Marker coordinate={results[1].coordinates}></Marker>}
-        {results[1] && <Marker coordinate={results[1].coordinates}><Text>{results[1].name}</Text></Marker>}
+        {results[1] && <Marker coordinate={{latitude: results[1].geocodes.main.latitude, longitude: results[1].geocodes.main.longitude}}></Marker>}
+        {results[1] && <Marker coordinate={{latitude: results[1].geocodes.main.latitude-(latitudeDelta/35), longitude:results[1].geocodes.main.longitude}}><Text style={{fontWeight: 'bold'}}>{results[1].name}</Text></Marker>}
 
-        {results[2] && <Marker coordinate={results[2].coordinates}></Marker>}
-        {results[2] && <Marker coordinate={results[2].coordinates}><Text>{results[2].name}</Text></Marker>}
+        {results[2] && <Marker coordinate={{latitude: results[2].geocodes.main.latitude, longitude: results[2].geocodes.main.longitude}}></Marker>}
+        {results[2] && <Marker coordinate={{latitude: results[2].geocodes.main.latitude-(latitudeDelta/35), longitude:results[2].geocodes.main.longitude}}><Text style={{fontWeight: 'bold'}}>{results[2].name}</Text></Marker>}
 
-        {results[3] && <Marker coordinate={results[3].coordinates}></Marker>}
-        {results[3] && <Marker coordinate={results[3].coordinates}><Text>{results[3].name}</Text></Marker>}
+        {results[3] && <Marker coordinate={{latitude: results[3].geocodes.main.latitude, longitude: results[3].geocodes.main.longitude}}></Marker>}
+        {results[3] && <Marker coordinate={{latitude: results[3].geocodes.main.latitude-(latitudeDelta/35), longitude:results[3].geocodes.main.longitude}}><Text style={{fontWeight: 'bold'}}>{results[3].name}</Text></Marker>}
 
-        {results[4] && <Marker coordinate={results[4].coordinates}></Marker>}
-        {results[4] && <Marker coordinate={results[4].coordinates}><Text>{results[4].name}</Text></Marker>}
-
+        {results[4] && <Marker coordinate={{latitude: results[4].geocodes.main.latitude, longitude: results[4].geocodes.main.longitude}}></Marker>}
+        {results[4] && <Marker coordinate={{latitude: results[4].geocodes.main.latitude-(latitudeDelta/35), longitude:results[4].geocodes.main.longitude}}><Text style={{fontWeight: 'bold'}}>{results[4].name}</Text></Marker>}
 
       </MapView>}
       { results[0] && results[1] && results[2] && results[3] && results[4] && 
@@ -125,15 +145,14 @@ export default function Main() {
             data={results}
 
             renderItem={({item}) => (
-                <View>
-                    {/* <Text onPress={() => navigation.navigate('/restaurant')}>{item.name}, <Text style={{color: 'gray'}}>{item.location.address1}</Text></Text> */}
+              <View>
+                  {/* <Text onPress={() => navigation.navigate('/restaurant')}>{item.name}, <Text style={{color: 'gray'}}>{item.location.address1}</Text></Text> */}
 
-                    <Link style={{backgroundColor: 'white', fontSize: 19}} href="/restaurant/"><Text>{item.name}, <Text style={{color: 'gray'}}>{item.location.address1}</Text></Text></Link>
-                </View>
+                  <Link style={{backgroundColor: 'white', fontSize: 19}} href="/restaurant/"><Text>{item.name}, <Text style={{color: 'gray'}}>{item.location.address}</Text></Text></Link>
+              </View>
             )}
             ItemSeparatorComponent={ItemSeparator}
-            
-	    />  }
+	        />  }
       <StatusBar style="auto" />
     </View>
   );
@@ -152,5 +171,4 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: Dimensions.get('window').height * 0.07
   }
-
 });
